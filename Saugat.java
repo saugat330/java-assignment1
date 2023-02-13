ff3d1e90c89867f07d3142ba81ccec7e46b982e5
saugat330 <ssaugat349@gmail.com>
2023-02-13 18:13:31 +0545

    first assign

---
+++ Saugat.java
@@ -0,0 +1,23 @@
+public class Saugat {
+    public static void main(String[] args) {  
+        System.out.println("Name of the organization:Kamalphokhari Resturant and bar");
+
+        System.out.println(""); 
+        System.out.println("");  
+
+        System.out.println("Address:");
+        System.out.println("");  
+
+        System.out.println("----State:KM");
+        System.out.println("----Zip code:44600");
+        System.out.println("----Country:Nepal");
+
+        System.out.println("");    
+        System.out.println(""); 
+
+        System.out.println("Contact information: "); 
+        System.out.println("----Mobile number:9865382409"); 
+        System.out.println("----Email:kamalpokhari.rnb@gmail.com"); 
+
+    }
+}
